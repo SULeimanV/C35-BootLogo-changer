a105ae81fd6303e7ffdeba98303075793c2b00b8
SULeimanV <88591086+SULeimanV@users.noreply.github.com>
2024-11-28 00:14:14 +0300

    adding to other counts

--- decompress.py
+++ decompress.py
@@ -3,25 +3,27 @@ import gzip
 filePath= "logo.bin"  #our logo.bin
 
 file = open(filePath, "rb")
+logos_count = 0
+pad = 0
 
 
 def get_bytes(i):
 	g = int.from_bytes(logos[0][::-1])
 	go = g
 
-	with open(f"{str(byte[48+10:48+11],encoding='ASCII')}.bmp.gz", "wb") as logo_gz:
-		logo_gz.write(byte[48:48+g])
+	with open(f"{str(byte[pad+10:pad+11],encoding='ASCII')}.bmp.gz", "wb") as logo_gz:
+		logo_gz.write(byte[pad:pad+g])
 
-		with open(f"{str(byte[48+10:48+11],encoding='ASCII')}.bmp", "wb") as i_out_file:
-			i_out_file.write(gzip.decompress(byte[48:48+g]))
+		with open(f"{str(byte[pad+10:pad+11],encoding='ASCII')}.bmp", "wb") as i_out_file:
+			i_out_file.write(gzip.decompress(byte[pad:pad+g]))
 
 	for ii in range(1,i+1):
 		g+=int.from_bytes(logos[ii][::-1])
-		with open(f"{str(byte[48+10+go:48+11+go],encoding='ASCII')}.bmp.gz", "wb") as logo_gz:
-			logo_gz.write(byte[48+go:48+g])
+		with open(f"{str(byte[pad+10+go:pad+11+go],encoding='ASCII')}.bmp.gz", "wb") as logo_gz:
+			logo_gz.write(byte[pad+go:pad+g])
 
-			with open(f"{str(byte[48+10+go:48+11+go],encoding='ASCII')}.bmp", "wb") as i_out_file:
-				i_out_file.write(gzip.decompress(byte[48+go:48+g]))
+			with open(f"{str(byte[pad+10+go:pad+11+go],encoding='ASCII')}.bmp", "wb") as i_out_file:
+				i_out_file.write(gzip.decompress(byte[pad+go:pad+g]))
 		go = g
 
 
@@ -37,6 +39,7 @@ try:
 		raise NameError("Header not found")
 
 	logos_count = int.from_bytes(byte[2])
+	pad = 24 + 4 * logos_count 
 
 	for i in range(logos_count):
 		logos.append(byte[24+i*4:27+i*4]) #Sizes
